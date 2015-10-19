3a57b2f49106629ebe8fdd8f692e61f92ed9b439
Ken Wakita <wakita@is.titech.ac.jp>
2015-10-20 03:51:19 +0900

    Initial commit: an empty and obviously wrong answer

---
+++ src/puzzle.scala
@@ -0,0 +1,14 @@
+// パス: lx03/src/puzzle.scala
+// エンコーディング: UTF8
+
+package puzzle
+
+object Main {
+  def puzzle(): List[List[Int]] = {
+    Nil
+  }
+
+  def main() {
+    println("Answer = " + puzzle())
+  }
+}
---
+++ test/puzzle.scala
@@ -0,0 +1,13 @@
+// パス: lx02/test/puzzle.scala
+// エンコーディング: UTF8
+
+package puzzle
+
+import org.scalatest._
+import Main._
+
+class Test extends FlatSpec with Matchers {
+  "main" should "be forced to run" in { // テストのなかで強制的に main を実行させているだけで、あまり意味はありません。
+    main() should be (())
+  }
+}
