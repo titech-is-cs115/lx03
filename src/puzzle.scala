86b34118a7a5ec40b8dd207a8688cfedf89d4ab9
Ken Wakita <wakita@is.titech.ac.jp>
2015-10-20 04:34:31 +0900

    ひとまず「やる気のないプログラム」とテストを書いた。
    なんてテストしにくいプログラムなんだ。もしかして、プログラムの書き方を改善したら、テストが書き易くなるのかな？

--- src/puzzle.scala
+++ src/puzzle.scala
@@ -4,8 +4,25 @@
 package puzzle
 
 object Main {
-  def puzzle(): List[List[Int]] = {
+  /*
+   * 戦略：{ [a, b, c, d] in [0,9]^4 | constraint([a, b, c, d]) }
+   * つまり、[0, 9]の範囲の整数 a, b, c, d のすべての組のリスト(candidates)を生成し、それらのうち制約 constraint を充足するものを見つける。
+   */
+
+  def candidates(): List[List[Int]] = {
     Nil
+  }
+
+  def constraint(candidate: List[Int]): Boolean = {
+    true
+  }
+
+  def check(): List[List[Int]] = {
+    Nil
+  }
+
+  def puzzle(): List[List[Int]] = {
+    check()
   }
 
   def main() {
--- test/puzzle.scala
+++ test/puzzle.scala
@@ -1,4 +1,4 @@
-// パス: lx02/test/puzzle.scala
+// パス: lx03/test/puzzle.scala
 // エンコーディング: UTF8
 
 package puzzle
@@ -7,7 +7,43 @@ import org.scalatest._
 import Main._
 
 class Test extends FlatSpec with Matchers {
-  "main" should "be forced to run" in { // テストのなかで強制的に main を実行させているだけで、あまり意味はありません。
+
+  val theCandidates = candidates()
+
+  "candidates()" should "be [[0,0,0,0], [0,0,0,1], [0,0,0,2], ..., [9,9,9,9]]" in {
+    theCandidates should contain (List(0, 0, 0, 0))
+    theCandidates should contain (List(9, 9, 9, 9))
+    theCandidates should not contain (List(0, 0, 0, -1))
+    theCandidates should not contain (List(0, 0, 0, 10))
+  }
+
+  "constraint(a, b, c, d)" should "reflect the specification" in {
+    constraint(List(0, 0, 0, 0)) should be (false)
+    constraint(List(1, 0, 0, 0)) should be (false)
+    constraint(List(0, 1, 0, 0)) should be (false)
+    constraint(List(0, 0, 1, 0)) should be (false)
+    constraint(List(0, 0, 0, 1)) should be (false)
+    // と、結果が false になるテストは書いてみた
+    // でも、結果が true になるテストはどうする？
+    // もし書いたとしたら、それって、すでに結果が求まっているってこと？
+  }
+
+  def every(p: List[Int] => Boolean, list: List[List[Int]]): Boolean = {
+    list match {
+      case Nil => true
+      case first :: rest => p(first) && every(p, rest)
+    }
+  }
+
+  "check()" should "choose all the candidates that meet the constraint" in {
+    // check() の結果がすべて constraint を充足すること
+    every(constraint, check()) should be (true)
+
+    // candidates() のうち、constraint を充足するもので check() に漏れているものがないこと
+    // うっ、どうやって書く？？？
+  }
+
+  "main" should "is forced to run" in { // テストのなかで強制的に main を実行させているだけで、あまり意味はありません。
     main() should be (())
   }
 }
