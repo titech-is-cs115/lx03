426ddd6befd8512b01e96bccfe82cf3a1d73776a
Ken Wakita <wakita@is.titech.ac.jp>
2015-10-20 10:23:52 +0900

    最初の実装からの反省から作った二番目の実装
    かなりすっきりとしたが，satisfy の部分のテストは大丈夫だろうか．効率を優先するとテストがしにくくなる．

--- src/puzzle.scala
+++ src/puzzle.scala
@@ -4,8 +4,57 @@
 package puzzle
 
 object Main {
+  /*
+   * 戦略：さきほどはすべての組合せを列挙する部分と制約の記述にバグをいれてしまった．
+   * もっと簡単な実装を目指す．問題が小さいから，性能に与える影響は限定的ですが，
+   * 列挙によって，かなりな記憶領域を消費しています．また，一部で手抜きをしてリストの
+   * 連結を使ってしまっているので，ゴミも発生しています．それらの問題を抑制することも
+   * あわせて心掛けます．
+   */
+
+  def c(n: Int)(nums: List[Int]): Int = {
+    nums match {
+      case Nil => 0
+      case x :: l => (if (x == n) 1 else 0) + c(n)(l)
+    }
+  }
+
+  def counts(nums: List[Int]): List[Int] = {
+    val c1 = c(1)(nums)
+    val c2 = c(2)(nums)
+    val c3 = c(3)(nums)
+    val c_ = nums.length - (c1 + c2 + c3)
+    List(c1, c2, c3, c_)
+  }
+
+  val countsOnPaper = counts(List(1, 2, 3, 1, 3))
+
+  def satisfy(candidate: List[Int]): Boolean = {
+    (candidate, counts(candidate), countsOnPaper) match {
+      case (List(x1, x2, x3, x_), List(n1, n2, n3, n_), List(_n1, _n2, _n3, _n_)) =>
+        // 穴の値 = 穴の数字の集計 + 紙に書かれた数字の集計
+        x1 == n1 + _n1 &&
+        x2 == n2 + _n2 &&
+        x3 == n3 + _n3 &&
+        x_ == n_ + _n_
+    }
+  }
+
   def puzzle(): List[List[Int]] = {
-    Nil
+    def N = 10 * 10 * 10 * 10
+    def genCheck(i: Int, answers: List[List[Int]]): List[List[Int]] = {
+      if (i >= N) answers
+      else {
+        val x1 = i / 1000
+        val x2 = (i / 100) % 10
+        val x3 = (i / 10) % 10
+        val x4 = i % 10
+        val candidate = List(x1, x2, x3, x4)
+        if (satisfy(candidate)) genCheck(i + 1, candidate :: answers)
+        else genCheck(i + 1, answers)
+      }
+    }
+    genCheck(0, Nil)
   }
 
   def main() {
--- test/puzzle.scala
+++ test/puzzle.scala
@@ -1,4 +1,4 @@
-// パス: lx02/test/puzzle.scala
+// パス: lx03/test/puzzle.scala
 // エンコーディング: UTF8
 
 package puzzle
@@ -7,6 +7,18 @@ import org.scalatest._
 import Main._
 
 class Test extends FlatSpec with Matchers {
+  "counts" should "count correctly" in {
+    counts(Nil) should be (List(0, 0, 0, 0))
+    counts(List(1)) should be (List(1, 0, 0, 0))
+    counts(List(1, 1, 1, 1, 1)) should be (List(5, 0, 0, 0))
+    counts(List(2, 2, 2, 2, 2)) should be (List(0, 5, 0, 0))
+    counts(List(3, 3, 3, 3, 3)) should be (List(0, 0, 5, 0))
+    counts(List(0, 0, 0, 0, 0)) should be (List(0, 0, 0, 5))
+    counts(List(0, 4, 5, 6, 7, 8, 9)) should be (List(0, 0, 0, 7))
+
+    countsOnPaper should be (List(2, 1, 2, 0))
+  }
+
   "main" should "be forced to run" in { // テストのなかで強制的に main を実行させているだけで、あまり意味はありません。
     main() should be (())
   }
